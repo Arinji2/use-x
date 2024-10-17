08421d0ca83ae4cfd4532aa5ef624b3b86136378
Arinji2 <arinjaydhar205@gmail.com>
2024-10-17 23:17:44 +0530

    chore: cleanup imports

--- src/use-fetch/use-fetch.ts
+++ src/use-fetch/use-fetch.ts
@@ -1,4 +1,4 @@
-import { type Dispatch, type SetStateAction, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { type Dispatch, type SetStateAction, useCallback, useEffect, useState } from "react";
 
 export type UseFetchOptions = {
   immediate: boolean;
